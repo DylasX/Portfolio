2a627182c144f8b456fc16cd35faed62c64c798f
Miguel Angel Sierra Giraldo <miguel-sg01@hotmail.com>
2019-09-12 02:30:51 -0500

    portfolio submits

--- src/pages/index.js
+++ src/pages/index.js
@@ -222,12 +222,12 @@ class HomeIndex extends React.Component {
                       ></textarea>
                     </div>
                   </div>
+                  <ul className="actions">
+                    <li>
+                      <input type="submit" value="Send Message" />
+                    </li>
+                  </ul>
                 </form>
-                <ul className="actions">
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
               </div>
               <div className="4u 12u$(small)">
                 <ul className="labeled-icons">
